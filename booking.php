08124f50af36a847822af8901bf92004df3315b8
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2018-11-20 19:08:41 -0200

    Change booking.php

--- booking.php
+++ booking.php
@@ -1,27 +1,40 @@
 <?php
-    $to = 'rodolfog.peixoto@gmail.com';
-    $from = $_POST["fname"];
-    $to= $_POST["email"];
-    $to= $_POST["tel"];
-    
+$name = $_POST['name'];
+//pega os dados que foi digitado no ID name.
 
+$email = $_POST['email'];
+//pega os dados que foi digitado no ID email.
 
-    $headers = 'MIME-Version: 1.0' . "\r\n";
-    $headers .= "From: " . $fname . "\r\n"; // Sender's E-mail
-    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+$subject = "Formulário da landing page well"
+//pega os dados que foi digitado no ID sebject.
 
-    $message ='<table style="width:100%">
-        <tr>
-            <td>'.$fname.'  '.$email.'</td>
-        </tr>
-        
-    </table>';
+// $message = $_POST['message'];
+//pega os dados que foi digitado no ID message.
+$headers = "From: " . $name . "\r\n"; // Sender's E-mail
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 
-    if (@mail($to, $email, $message, $headers))
-    {
-        echo 'Entraremos em contato em breve.';
-    }else{
-        echo 'falhou';
-    }
+/*abaixo contém os dados que serão enviados para o email
+cadastrado para receber o formulário*/
 
+$corpo = "Formulário enviado\n";
+$corpo .= "Nome: " . $name . "\n";
+$corpo .= "Email: " . $email . "\n";
+
+$email_to = 'seu e-mail aqui que irá receber a mensagem';
+//não esqueça de substituir este email pelo seu.
+
+$status = mail($email_to, $subject, $corpo, $headers);
+//enviando o email.
+
+if ($status) {
+  echo "<script> alert('Formulário enviado com sucesso!'); </script>";
+  
+//mensagem de form enviado com sucesso.
+
+} else {
+  echo "<script> alert('Falha ao enviar o Formulário.'); </script>";
+  
+//mensagem de erro no envio. 
+
+}
 ?>
